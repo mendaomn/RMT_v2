f7267dcd0e52c3c958ae8d2ab6017f916097e874
Alessandro Menduni <menduni6@gmail.com>
2015-11-01 15:58:26 +0100

    Add sections and food pages

---
+++ js/controllers/foodList.js
@@ -0,0 +1,13 @@
+// foodList.js
+
+(function() {
+    var app = angular.module('Food', ['Services']);
+    app.controller('FoodCtrl', ['$scope', 'menu', function($scope, menu) {
+        $scope.foodList = menu.getFoodList($scope.appstate.section);
+
+        $scope.selectSection = function(section) {
+            $scope.appstate.section = section;
+            $scope.appstate.currentView = 'foodList';
+        };
+    }]);
+})();
---
+++ js/controllers/sectionsList.js
@@ -0,0 +1,15 @@
+// sectionsList.js
+
+(function() {
+    var app = angular.module('Sections', ['Services']);
+    app.controller('SectionsCtrl', ['$scope', 'menu', function($scope, menu) {
+        menu.getSections().then(function(sections) {
+            $scope.sections = sections;
+        });
+
+        $scope.selectSection = function(section) {
+            $scope.appstate.section = section;
+            $scope.appstate.currentView = 'food';
+        };
+    }]);
+})();
