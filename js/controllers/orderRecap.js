d02605dd950586e1807c7ac70e4738570ea9720b
Alessandro Menduni <menduni6@gmail.com>
2015-12-20 13:18:04 +0100

    Enhance orderRecap, add ability to take notes on items

--- js/controllers/orderRecap.js
+++ js/controllers/orderRecap.js
@@ -3,13 +3,16 @@
 (function() {
     var app = angular.module('OrderRecap', ['Services']);
     app.controller('OrderCtrl', ['$scope', function($scope) {
+        // Init $scope to hold the current order
         $scope.orderContent = $scope.appstate.order.getContent();
 
+        // Handler: removes item from order
         $scope.removeFood = function(orderItem) {
             console.log(orderItem, "removing!");
             $scope.appstate.order.removeItem(orderItem);
         };
 
+        // Handler: adds 1 to item's quantity
         $scope.orderFood = function(orderItem) {
             $scope.appstate.order.addFood({
                 food: orderItem.food,
@@ -17,12 +20,27 @@
             });
         };
 
+        // Handler: reduces by 1 from item's quantity
         $scope.reduceFood = function(orderItem) {
             $scope.appstate.order.reduceFood(orderItem.food);
         };
 
+        // Handler: shows note taker or something
         $scope.showNote = function(orderItem) {
+            // Create the note's environment in the $scope  
+            $scope.note = {
+                visible: true,
+                item: orderItem
+            };
+
             console.log("Please add note :D", orderItem.food.name, orderItem.quantity);
         };
+
+        // Save note content to the order's item
+        // It should do more
+        $scope.saveNote = function(){
+            $scope.note.item.note = $scope.note.text;
+        };
+
     }]);
 })();
